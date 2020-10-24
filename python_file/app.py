ed2959c1abee70324d7b3cac13f88319e7c08fe1
Tito <titowindler01@gmail.com>
2020-10-24 20:56:03 +0800

    third commit

---
+++ python_file/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/api',methods=['GET'])
+def hello_world():
+	d={}
+	d['Search']=str(request.args['Query'])
+	return jsonify(d)
+
+
+if __name__ == "__main__":
+	app.run(debug = true)
